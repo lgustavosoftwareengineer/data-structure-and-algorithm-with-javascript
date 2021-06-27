70ece799ccde0f50cd0131bf070517048909fcff
Luiz Gustavo <luiz.matias@upe.br>
2021-06-27 14:57:28 -0300

    feat(core/linkedlist): implement the `LinkedList` insertion method

--- src/core/LinkedList/linked-list.js
+++ src/core/LinkedList/linked-list.js
@@ -22,7 +22,24 @@ export default class LinkedList {
     }
     this.count++;
   }
-  insert(element, position) {}
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        node.next = current;
+        previous.next = node;
+        this.count++;
+        return true;
+      }
+    }
+    return false;
+  }
   getElementAt(index) {
     if (index >= 0 && index <= this.count) {
       let current = this.head;
