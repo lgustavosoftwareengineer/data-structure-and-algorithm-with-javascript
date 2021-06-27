d6b0f7b670a1949f90ec60718d2ec8cfaf466d82
Luiz Gustavo <luiz.matias@upe.br>
2021-06-27 14:22:42 -0300

    feat(core/linkedlist): implement the getElementAt and removeAt methods

--- src/core/LinkedList/linked-list.js
+++ src/core/LinkedList/linked-list.js
@@ -28,9 +28,35 @@ export default class LinkedList {
     return this.head === undefined ? pushAtFirst() : pushAtSomewhere();
   }
   insert(element, position) {}
-  getElementAt(index) {}
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let current = this.head;
+
+      for (let i = 1; i < index; i++) {
+        current = current.next;
+      }
+
+      return current;
+    } else {
+      return undefined;
+    }
+  }
   remove(element) {}
-  removeAt(index) {}
+  removeAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let current = this.head;
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
   indexOf(element) {}
   isEmpty() {}
   size() {}
--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import { LinkedList } from "./core";
 const linkedList = new LinkedList();
 linkedList.push(2);
 linkedList.push(3);
-
-console.log(linkedList);
+console.log(`removeAt: ${linkedList.removeAt(1)}`);
+console.log(`linkedList: ${linkedList}`);
